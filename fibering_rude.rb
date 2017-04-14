6b766a3d164424a3ce75a6090bdcfd6fe9a679c1
Ed Toro <ed@wyncode.co>
2017-04-14 14:50:05 -0400

    remove concurrency testing stuff (for now)

--- fibering_rude.rb
+++
@@ -1,9 +0,0 @@
-puts "start"
-
-f1 = Fiber.new { 5.times { puts 'hello' } }
-
-f2 = Fiber.new { 5.times { puts 'world'} }
-
-[f1, f2].map(&:resume)
-
-puts "end"
--- threading.rb
+++
@@ -1,8 +0,0 @@
-puts "start"
-
-t1 = Thread.new { 5.times { puts "hello" } }
-
-t2 = Thread.new { 5.times { puts "world" } }
-
-[t1, t2].map(&:join)
-puts "end"
